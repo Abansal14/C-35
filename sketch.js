2c3296f18ea7ce41be299dfdc976549871d925e4
Abansal14 <78475065+Abansal14@users.noreply.github.com>
2021-07-08 12:50:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var ball;
+var database;   //to create the database
+var ballPositionRef;    //refer to the position value in the database
+var position;
+
+function setup(){
+    createCanvas(500,500);
+
+    //create the database inside the database variable --> firebase.database()
+    database=firebase.database()
+    
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    //refer to the database values --> database.ref('')
+    ballPositionRef = database.ref('box/position');
+
+    //listen to the changes that are happening inside the database --> var.on()
+    ballPositionRef.on("value",readPosition, showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('box/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readPosition(data){        //data --> listened information
+//store the llistened info inside the position
+position = data.val();
+
+ball.x  = position.x ; 
+ball.y = position.y;
+}
+
+function showError(){
+    console.log("There is an error")
+    
+}
